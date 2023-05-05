7974964cc434473447b35c4ea5e1362b3c40a3a7
Akam Programist <akadev404@gmail.com>
2023-05-05 23:57:36 +0300

    commits

--- pages/Resume.vue
+++ pages/Resume.vue
@@ -3,23 +3,24 @@
     <div>
       <p class="text-2xl font-semibold my-10">My Resume</p>
     </div>
-    <div class="overflow-x-scroll">
-      <div class="flex items-center text-lg bg-black rounded-full">
-        <nuxt-link
-          to=""
-          class="bg-creamy flex-1 text-primary py-2 px-8 font-semibold rounded-full"
-          >SKILLS</nuxt-link
-        >
-        <nuxt-link to="" class="flex-1 py-2 px-8 font-semibold rounded-full"
-          >PROJECTS</nuxt-link
-        >
-        <nuxt-link to="" class="flex-1 py-2 px-8 font-semibold rounded-full"
-          >EXPERIENCE</nuxt-link
-        >
-        <nuxt-link to="" class="flex-1 py-2 px-8 font-semibold rounded-full"
-          >EDUCATION</nuxt-link
-        >
-      </div>
+
+    <div
+      class="grid rounded-3xl md:py-0 grid-cols-3 md:flex md:rounded-full md:text-lg items-center bg-black"
+    >
+      <nuxt-link
+        to=""
+        class="bg-creamy flex-1 text-primary py-3 px-8 font-semibold rounded-full"
+        >SKIILS</nuxt-link
+      >
+      <nuxt-link to="" class="flex-1 py-2 px-8 font-semibold rounded-full"
+        >PROJECTS</nuxt-link
+      >
+      <nuxt-link to="" class="flex-1 py-2 px-8 font-semibold rounded-full"
+        >EXPIRENCE</nuxt-link
+      >
+      <nuxt-link to="" class="flex-1 py-2 px-8 font-semibold rounded-full"
+        >EDUCATION</nuxt-link
+      >
     </div>
   </div>
 </template>
