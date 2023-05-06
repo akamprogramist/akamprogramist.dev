da400a8cb7fda046188c0f03519be7a5147d11e8
Akam Programist <akadev404@gmail.com>
2023-05-06 21:14:17 +0300

    commits

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <Landing />
+    <!-- <Landing />
     <About />
-    <Projects />
+    <Projects /> -->
+    <Tab>hello from tab 1</Tab>
+    <Tab>hello from tab 2</Tab>
+    <Tab>hello from tab 3</Tab>
+    <Tab>hello from tab 4</Tab>
   </div>
 </template>
