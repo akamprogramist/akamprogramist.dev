9ab35a758d25b9146ee1ee5fde66a8832e1ca25d
Akam Programist <akadev404@gmail.com>
2023-09-23 10:49:11 +0300

    commits

--- components/About.vue
+++ components/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-20" id="about">
+  <div class="pt-20" id="about">
     <Sticky />
     <div class="py-10 gap-10">
       <div>
@@ -97,73 +97,74 @@
           </div>
         </div>
       </div>
-      <!-- <div>
-        <div
-          class="grid grid-cols-3 items-center align-top after:h-[1px] after:col-span-2 after:block after:bg-white"
-        >
-          <p class="tracking-[4px] font-bold">Language</p>
-        </div>
-        <div class="my-10 space-y-3 text-stone-400">
-          <div class="flex items-center text-xl space-x-2">
-            <Icon
-              name="material-symbols:arrow-right"
-              class="text-2xl text-creamy"
-            />
-            <p>English</p>
-          </div>
-          <div class="flex items-center text-xl space-x-2">
-            <Icon
-              name="material-symbols:arrow-right"
-              class="text-2xl text-creamy"
-            />
-            <p>Turkish</p>
-          </div>
-          <div class="flex items-center text-xl space-x-2">
-            <Icon
-              name="material-symbols:arrow-right"
-              class="text-2xl text-creamy"
-            />
-            <p>Persian</p>
-          </div>
-          <div class="flex items-center text-xl space-x-2">
-            <Icon
-              name="material-symbols:arrow-right"
-              class="text-2xl text-creamy"
-            />
-            <p>Kurdish</p>
-          </div>
-        </div>
-      </div> -->
-      <!-- <div>
-        <div
-          class="grid grid-cols-3 items-center align-top after:h-[1px] after:col-span-2 after:bg-white"
-        >
-          <p class="tracking-[4px] font-bold">Experience</p>
-        </div>
-        <div class="my-10 space-y-8 text-stone-400">
-          <div class="flex items-center text-xl space-x-3">
-            <Icon
-              name="material-symbols:arrow-right"
-              class="text-2xl text-creamy"
-            />
-            <div class="space-y-1">
-              <p>Fron-end Web developer</p>
-              <p class="text-sm">HCDT Company</p>
-            </div>
-          </div>
-          <div class="flex items-center text-xl space-x-3">
-            <Icon
-              name="material-symbols:arrow-right"
-              class="text-2xl text-creamy"
-            />
-            <div class="space-y-1">
-              <p>KivaKurd Fullstack Website</p>
-              <p class="text-sm">Tech Fest</p>
-            </div>
-          </div>
-        </div>
-      </div> -->
+
+      <!-- languages -->
+      <!--
+                  <div
+                    class="grid grid-cols-3 items-center align-top after:h-[1px] after:col-span-2 after:block after:bg-white"
+                  >
+                    <p class="tracking-[4px] font-bold">Language</p>
+                  </div>
+                  <div class="my-10 space-y-3 text-stone-400">
+                    <div class="flex items-center text-xl space-x-2">
+                      <Icon
+                        name="material-symbols:arrow-right"
+                        class="text-2xl text-creamy"
+                      />
+                      <p>English</p>
+                    </div>
+                    <div class="flex items-center text-xl space-x-2">
+                      <Icon
+                        name="material-symbols:arrow-right"
+                        class="text-2xl text-creamy"
+                      />
+                      <p>Turkish</p>
+                    </div>
+                    <div class="flex items-center text-xl space-x-2">
+                      <Icon
+                        name="material-symbols:arrow-right"
+                        class="text-2xl text-creamy"
+                      />
+                      <p>Persian</p>
+                    </div>
+                    <div class="flex items-center text-xl space-x-2">
+                      <Icon
+                        name="material-symbols:arrow-right"
+                        class="text-2xl text-creamy"
+                      />
+                      <p>Kurdish</p>
+                    </div>
+                  </div>
+                 -->
+
+      <!-- Expirence -->
+      <!-- <div
+                    class="grid grid-cols-3 items-center align-top after:h-[1px] after:col-span-2 after:bg-white"
+                  >
+                    <p class="tracking-[4px] font-bold">Experience</p>
+                  </div>
+                  <div class="my-10 space-y-8 text-stone-400">
+                    <div class="flex items-center text-xl space-x-3">
+                      <Icon
+                        name="material-symbols:arrow-right"
+                        class="text-2xl text-creamy"
+                      />
+                      <div class="space-y-1">
+                        <p>Fron-end Web developer</p>
+                        <p class="text-sm">HCDT Company</p>
+                      </div>
+                    </div>
+                    <div class="flex items-center text-xl space-x-3">
+                      <Icon
+                        name="material-symbols:arrow-right"
+                        class="text-2xl text-creamy"
+                      />
+                      <div class="space-y-1">
+                        <p>KivaKurd Fullstack Website</p>
+                        <p class="text-sm">Tech Fest</p>
+                      </div>
+                    </div>
+                  </div> -->
     </div>
   </div>
-  <div></div>
 </template>
