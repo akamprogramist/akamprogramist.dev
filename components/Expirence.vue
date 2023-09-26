2aa1079b03f6eb5092b418a29614de46210e6793
Akam Programist <akadev404@gmail.com>
2023-09-26 17:31:59 +0300

    commits

--- components/Expirence.vue
+++ components/Expirence.vue
@@ -20,16 +20,50 @@
             class="flex items-center gap-2 text-sm text-bluesh dark:text-creamy"
           >
             <Icon name="uis:schedule" />
-            <p>2017 - 2019</p>
+            <p>2020 - 2020</p>
           </div>
-          <p class="font-extrabold text-lg">Senior UI Designer</p>
-          <p class="text-sm dark:text-stone-400 text-stone-700">
-            Envato Market
-          </p>
+          <p class="font-extrabold text-lg capitalize">technical specialist</p>
+          <p class="text-sm dark:text-stone-400 text-stone-700">HCDT Company</p>
         </div>
         <p class="mt-5 dark:text-stone-400 text-stone-700">
-          This article will show you, step by step, how to build a chatbot that
-          meets your business goal will enable us to create impressive
+          I was a Full-stack web developer. My stack was Vue js, express js,
+          Node js and MongoDB. I helped them make their website more user
+          friendly and helped them with the product management and technical
+          issues at the company.
+        </p>
+      </div>
+    </div>
+    <div class="flex items-start">
+      <div class="mr-5">
+        <div class="p-4 bg-bluesh dark:bg-creamy rounded-full">
+          <Icon
+            name="uis:schedule"
+            class="text-2xl text-white dark:text-black"
+          />
+        </div>
+        <div
+          class="border-l-2 ml-[26px] border-bluesh dark:border-creamy h-[225px]"
+        ></div>
+      </div>
+      <div
+        class="bg-[#f0e7db] dark:bg-primary border-zinc-400 dark:border-stone-700 border p-10 rounded-lg"
+      >
+        <div class="capitalize">
+          <div
+            class="flex items-center gap-2 text-sm text-bluesh dark:text-creamy"
+          >
+            <Icon name="uis:schedule" />
+            <p>2022 - 2022</p>
+          </div>
+          <p class="font-extrabold text-lg capitalize">
+            competition participate
+          </p>
+          <p class="text-sm dark:text-stone-400 text-stone-700">KivaKurd</p>
+        </div>
+        <p class="mt-5 dark:text-stone-400 text-stone-700">
+          I was one of participants of Techfest and I had a project about giving
+          loan to poor people and it was a website I used PHP, Laravel,
+          tailwindcss and Vuejs for it.
         </p>
       </div>
     </div>
@@ -50,16 +84,17 @@
             class="flex items-center gap-2 text-sm text-bluesh dark:text-creamy"
           >
             <Icon name="uis:schedule" />
-            <p>2017 - 2019</p>
+            <p>2022 - 2022</p>
           </div>
-          <p class="font-extrabold text-lg">Senior UI Designer</p>
+          <p class="font-extrabold text-lg capitalize">full-stack developer</p>
           <p class="text-sm dark:text-stone-400 text-stone-700">
-            Envato Market
+            Hedgehog Agency
           </p>
         </div>
         <p class="mt-5 dark:text-stone-400 text-stone-700">
-          This article will show you, step by step, how to build a chatbot that
-          meets your business goal will enable us to create impressive
+          I was a Full-stack web developer for hedgehog company which is a
+          digital marketing agency I was making their Enterprise websites. I
+          helped them create some of premium websites for their clients
         </p>
       </div>
     </div>
